d886813440866e811899f0271e4cade5c00776d0
dairvin9 <dairvin9@tamu.edu>
2016-06-04 17:03:03 -0500

    remove files

--- app/views.py
+++ app/views.py
@@ -2,6 +2,8 @@ from flask import render_template, flash, redirect
 from app import app
 from .forms import CommentForm
 
+# Sets up the url and functions that load html pages
+
 @app.route('/')
 @app.route('/index')
 def index():
